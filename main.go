6113497fbcdc168ce2c4cb0cefa15cc427ba281c
u1and0 <e01.ando60@gmail.com>
2022-03-17 22:28:39 +0900

    [mod] parseExcel data method

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 )
 
 const (
-	DEBUG    = true
-	LAYOUT   = "2006/01/02"
-	SKIPROWS = 2
+	DEBUG      = true
+	LAYOUT     = "2006/01/02"
+	SKIPHEADER = 2
 )
 
 type (
@@ -26,6 +26,33 @@ type (
 	}
 	UserData []UserDatum
 )
+
+func (d *UserData) parseExcel(rows [][]string) error {
+	for i, row := range rows {
+		if i < SKIPHEADER {
+			continue
+		}
+		r0, err := time.Parse(LAYOUT, row[0])
+		if err != nil {
+			return err
+		}
+		r6, err := time.Parse(LAYOUT, row[6])
+		if err != nil {
+			return err
+		}
+		r4, err := strconv.Atoi(row[4])
+		if err != nil {
+			return err
+		}
+		r5, err := strconv.Atoi(row[5])
+		if err != nil {
+			return err
+		}
+		datum := UserDatum{r0, row[1], row[2], row[3], r4, r5, r6}
+		*d = append(*d, datum)
+	}
+	return nil
+}
 
 func main() {
 	f, err := excelize.OpenFile("sample_data.xlsx")
@@ -46,17 +73,7 @@ func main() {
 		return
 	}
 	data := UserData{}
-	for i, row := range rows {
-		if i < 2 {
-			continue
-		}
-		r0, _ := time.Parse(LAYOUT, row[0])
-		r6, _ := time.Parse(LAYOUT, row[6])
-		r4, _ := strconv.Atoi(row[4])
-		r5, _ := strconv.Atoi(row[5])
-		datum := UserDatum{r0, row[1], row[2], row[3], r4, r5, r6}
-		data = append(data, datum)
-	}
+	err = data.parseExcel(rows)
 	if DEBUG {
 		fmt.Printf("%v\n", data)
 	}
